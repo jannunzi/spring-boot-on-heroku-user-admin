e6249e1f46e7ace87eb1eec229ee2ee54470d42a
Jose Annunziato <jannunzi@gmail.com>
2018-05-04 10:54:35 -0400

    added create user

--- src/main/java/webdev/services/UserService.java
+++ src/main/java/webdev/services/UserService.java
@@ -2,6 +2,8 @@ package webdev.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import webdev.model.*;
 import webdev.repository.UserRepository;
@@ -16,5 +18,10 @@ public class UserService {
 	@GetMapping("/api/user")
 	public List<User> findAllUsers() {
 		return repository.findAll();
+	}
+
+	@PostMapping("/api/user")
+	public User createUser(@RequestBody User user) {
+		return repository.save(user);
 	}
 }
